903bebc2b4c77a1b811b7a3fd6c2646e71e86c5d
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-04-22 10:45:33 +0200

    Ajout de la dépendance postcss-import en version 16.1.0, mise à jour de la configuration des écrans dans tailwind.config.js, et modification de l'importation de Tailwind CSS dans index.css pour utiliser les directives @tailwind.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,6 +17,13 @@ export default {
           light: '#2e2e2e',
         },
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
